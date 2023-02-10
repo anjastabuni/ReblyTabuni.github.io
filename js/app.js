434be15263573b58d12dbe0c69ea17dbde7b0e7c
username_anjastabuni <reblytabuni2004@gmail.com>
2023-02-10 07:53:43 +0700

    up web profil design with bootstrap 5.x

---
+++ js/app.js
@@ -0,0 +1,16 @@
+function redirInstagram() {
+  window.location.href = "https://instagram.com/mekal.tabuni";
+}
+
+function redirWhatsapp() {
+  window.location.href = "https://wa.me/+6281248069721";
+}
+function redirGithub() {
+  window.location.href = "https://github.com/anjastabuni";
+}
+function redirLinkend() {
+  window.location.href = "https://linkedin.com/reblytabuni22";
+}
+function redirEmail() {
+  window.location.href = "https://mail.google.com";
+}
