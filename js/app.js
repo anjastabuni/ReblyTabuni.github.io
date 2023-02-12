f07c163d9c40a3cc2b182edc4af52ba0f077d8b1
username_anjastabuni <reblytabuni2004@gmail.com>
2023-02-12 17:00:19 +0700

    desain proggress skill

--- js/app.js
+++ js/app.js
@@ -1,3 +1,12 @@
+var acc = document.getElementsByClassName("detail");
+var d;
+for (d = 0; d < acc.length; d++) {
+  acc[d].onclick = function () {
+    this.classList.toggle("active");
+    this.nextElementSibling.classList.toggle("show");
+  };
+}
+
 function redirInstagram() {
   window.location.href = "https://instagram.com/mekal.tabuni";
 }
