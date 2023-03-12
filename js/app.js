107094414b3a229cbbac83ea9d3ac879fb9be73f
username_anjastabuni <reblytabuni2004@gmail.com>
2023-03-12 17:03:51 +0700

    tambah mode terang & gelap

--- js/app.js
+++ js/app.js
@@ -1,12 +1,3 @@
-var acc = document.getElementsByClassName("detail");
-var d;
-for (d = 0; d < acc.length; d++) {
-  acc[d].onclick = function () {
-    this.classList.toggle("active");
-    this.nextElementSibling.classList.toggle("show");
-  };
-}
-
 function redirInstagram() {
   window.location.href = "https://instagram.com/mekal.tabuni";
 }
@@ -18,7 +9,7 @@ function redirGithub() {
   window.location.href = "https://github.com/anjastabuni";
 }
 function redirLinkend() {
-  window.location.href = "https://linkedin.com/reblytabuni22";
+  window.location.href = "https://linkedin.com/in/reblytabuni22/";
 }
 function redirEmail() {
   window.location.href = "https://mail.google.com";
