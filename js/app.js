63c1f011a816dace929f774b0d12d339e12f923a
username_anjastabuni <reblytabuni2004@gmail.com>
2023-03-16 11:21:22 +0700

    tambah gambar

--- js/app.js
+++ js/app.js
@@ -1,3 +1,12 @@
+var acc = document.getElementsByClassName("detail");
+var d;
+for (d = 0; d < acc.length; d++) {
+  acc[d].onclick = function () {
+    this.classList.toggle("active");
+    this.nextElementSibling.classList.toggle("show");
+  };
+}
+
 function redirInstagram() {
   window.location.href = "https://instagram.com/mekal.tabuni";
 }
@@ -9,7 +18,7 @@ function redirGithub() {
   window.location.href = "https://github.com/anjastabuni";
 }
 function redirLinkend() {
-  window.location.href = "https://linkedin.com/in/reblytabuni22/";
+  window.location.href = "https://linkedin.com/reblytabuni22";
 }
 function redirEmail() {
   window.location.href = "https://mail.google.com";
